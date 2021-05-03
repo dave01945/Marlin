9d6e8f7a7fb8c12e3dcd768974e90222f7e63c8e
david <dave01945@gmail.com>
2021-05-03 08:50:18 +0100

    Hybrid threshhold

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -987,7 +987,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, 0, -0.15 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, 0, -0.10 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
